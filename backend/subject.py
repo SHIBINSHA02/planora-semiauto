4a298e27ed97d93bae934c9afa240b5a55b8a8d6
jydv402 <jydv402@gmail.com>
2025-09-25 16:11:45 +0530

    updated schema

--- backend/subject.py
+++ backend/subject.py
@@ -1,15 +1,14 @@
 class Subject:
-    def __init__(self, subject_id, name, is_lab, periods_per_session):
+    def __init__(self, subject_id, name):
         self.subject_id = subject_id
         self.name = name
-        self.is_lab = is_lab
-        self.periods_per_session = periods_per_session
+        # self.is_lab = is_lab
 
     def info_getter(self):
         return {
             "subject_id": self.subject_id,
             "name": self.name,
-            "is_lab": self.is_lab,
+            # "is_lab": self.is_lab,
             "periods_per_session": self.periods_per_session
         }
 
